ca648eb04d5863edb5d2b8a00fbc9ca12e89ec8a
Mika Heino <mika.heino@recordlydata.com>
2024-08-30 11:03:29 +0000

    Adding new staging tables

--- models/staging/tpch/stg_tpch__orders.sql
+++ models/staging/tpch/stg_tpch__orders.sql
@@ -6,17 +6,17 @@
 }}
 
 select 
-    o_orderkey,
-    o_custkey,
-    o_orderstatus,
-    o_totalprice,
-    o_orderdate,
-    o_orderpriority,
-    o_clerk,
-    o_shippriority,
-    o_comment,
-    1 as a
-  from {{ source('tpch','orders') }}
+        o_orderkey as order_key,
+        o_custkey as customer_key,
+        o_orderstatus as status_code,
+        o_totalprice as total_price,
+        o_orderdate as order_date,
+        o_orderpriority as priority_code,
+        o_clerk as clerk_name,
+        o_shippriority as ship_priority,
+        o_comment as comment
+        
+  from {{ source('tpch_sf1','orders') }}
 
 {% if is_incremental() %}
 
